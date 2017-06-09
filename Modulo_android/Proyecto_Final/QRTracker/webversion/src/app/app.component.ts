25ddcbb0f848b133c43cf0cd145baaf281e92ebf
DaielChom <danielcarvajalpatino@gmail.com>
2017-06-09 18:45:28 -0500

    Creando estructura principal

---
+++ Modulo_android/Proyecto_Final/QRTracker/webversion/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.css'],
+})
+export class AppComponent  { name = 'Angular'; }
